1c0634e19e09a3e303d06477012ac423d496f7ae
elfenlaid <cursed.elfenlaid@gmail.com>
2018-04-21 21:28:00 +0300

    prettify and progress track fixes

--- URLSession/ViewController.swift
+++ URLSession/ViewController.swift
@@ -8,58 +8,51 @@
 
 import UIKit
 
-let url = URL(string: "https://cs9-16v4.vkuseraudio.net/p20/1685a0e6d49bfd.mp3?extra=tpoxexMkTaCpCGfZ2524uIdyIeV5TF-PyDYuXw5BmCXYULZNuXSxVVmI-uIoG_ROnm4ViLakkd2Vio4aFVFuxBkB5yJ99x9EYJel9IwnI6lDMu-Qw7HaPOGlaTJHkYD9AWR7dIlFIAg7")
+let url = URL(string: "https://cs9-16v4.vkuseraudio.net/p20/1685a0e6d49bfd.mp3?extra=tpoxexMkTaCpCGfZ2524uIdyIeV5TF-PyDYuXw5BmCXYULZNuXSxVVmI-uIoG_ROnm4ViLakkd2Vio4aFVFuxBkB5yJ99x9EYJel9IwnI6lDMu-Qw7HaPOGlaTJHkYD9AWR7dIlFIAg7")!
 
 class ViewController: UIViewController, URLSessionDelegate, URLSessionDownloadDelegate{
     
-    var downloader: URLDownloader?
-    var progress: Progress?
+    var downloader = URLDownloader(with: url)
+    var progress = Progress(totalUnitCount: 0)
     let button: UIButton = {
-        let button = UIButton(frame: CGRect(x: 50, y: 400, width: 100, height: 50))
-        button.setTitleColor(.black, for: .normal)
-        button.setTitle("download", for: .normal)
-        
+        let button = UIButton(type: .system)
+        button.setTitle("Download", for: .normal)
         button.addTarget(self, action: #selector(download), for: .touchUpInside)
         return button
     }()
     
-    let progressBar: UIProgressView = {
+    lazy var progressBar: UIProgressView = {
         let progressView = UIProgressView(progressViewStyle: .bar)
-        progressView.frame = CGRect(x: 0, y: 100, width: UIScreen.main.bounds.width, height: 75)
-        progressView.trackTintColor = .red
-        progressView.progress = 0
-        progressView.progressTintColor = .gray
+        progressView.trackTintColor = .lightGray
+        progressView.observedProgress = self.progress
         return progressView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        downloader = URLDownloader(with: url!)
-        self.view.addSubview(progressBar)
-        self.view.addSubview(button)
+        view.addSubview(progressBar)
+        view.addSubview(button)
+
+        progressBar.frame = CGRect(x: 0, y: 100, width: UIScreen.main.bounds.width, height: 75)
+        button.frame = CGRect(x: 50, y: 400, width: 100, height: 50)
     }
     
     @objc func download(){
-        downloader?.download()
+        downloader.download(delegate: self)
     }
     
     //MARK:URLSession delegate conforming
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        downloadTask.cancel()
-        session.finishTasksAndInvalidate()
+        progress.completedUnitCount = 0
+        progress.totalUnitCount = 0
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        progress = Progress(totalUnitCount: totalBytesExpectedToWrite)
-        if progress != nil{
-            progress!.completedUnitCount = totalBytesWritten
-            print(progress!)
-            DispatchQueue.main.async { //Не обновляется прогресс =(
-                self.progressBar.observedProgress = self.progress!
-            }
+        if progress.totalUnitCount == 0 {
+            progress.totalUnitCount = totalBytesExpectedToWrite
         }
+
+        progress.completedUnitCount = progress.completedUnitCount + bytesWritten
     }
-    
-    
 }
 
